0b35e1e46474d9c8c431f366c4654340fbdcee98
Bartosz Wierzbicki (PGS Software) <bartosz@wierzbicki.biz>
2020-02-13 14:31:11 +0100

    additional stuff

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -21,70 +21,81 @@ class _CounterState extends State<Counter> {
     Colors.yellow
   ];
 
+  var _bgColors = [
+    Colors.lightGreen,
+    Colors.lightBlueAccent,
+    Colors.cyan,
+    Colors.grey,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          'Consumer',
-          style: _headerStyle,
-        ),
-        Consumer<provider.Counter>(
-          builder: (_, counter, __) {
-            return Column(
-              children: <Widget>[
-                Text(
-                  counter.count.toString(),
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: _colors[_rng.nextInt(_colors.length - 1)],
+    return Container(
+      color: _bgColors[_rng.nextInt(_bgColors.length)],
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Consumer',
+            style: _headerStyle,
+          ),
+          Consumer<provider.Counter>(
+            builder: (_, counter, __) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    counter.count.toString(),
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: _colors[_rng.nextInt(_colors.length)],
+                    ),
                   ),
-                ),
-                Text(
-                  counter.count2.toString(),
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: _colors[_rng.nextInt(_colors.length - 1)],
+                  Text(
+                    counter.count2.toString(),
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: _colors[_rng.nextInt(_colors.length)],
+                    ),
                   ),
-                ),
-              ],
-            );
-          },
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Selector',
-              style: _headerStyle,
-            ),
-            Selector<provider.Counter, int>(
-              selector: (_, countProvider) => countProvider.count,
-              builder: (_, data, __) {
-                return Text(
-                  data.toString(),
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: _colors[_rng.nextInt(_colors.length - 1)],
-                  ),
-                );
-              },
-            ),
-            Selector<provider.Counter, String>(
-              selector: (_, countProvider) =>
-                  countProvider.count2 > 5 ? 'więcej niż 5' : 'mniej niż 5',
-              builder: (_, data, __) {
-                return Text(
-                  data,
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: _colors[_rng.nextInt(_colors.length - 1)],
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ],
+                ],
+              );
+            },
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Selector',
+                style: _headerStyle,
+              ),
+              Selector<provider.Counter, int>(
+                selector: (_, countProvider) => countProvider.count,
+                builder: (_, data, __) {
+                  return Text(
+                    data.toString(),
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: _colors[_rng.nextInt(_colors.length)],
+                    ),
+                  );
+                },
+              ),
+              Selector<provider.Counter, String>(
+                selector: (_, countProvider) =>
+                    countProvider.count2 > 5 ? 'więcej niż 5' : 'mniej niż 5',
+                builder: (_, data, __) {
+                  return Text(
+                    data,
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: _colors[_rng.nextInt(_colors.length)],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          Text('Tło całości zmienia się na 15 w counter 1, a to zostaje'),
+        ],
+      ),
     );
   }
 }
